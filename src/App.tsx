b78d79ea358d01475e4b9f38547bdede228129d4
Douglas Oliveira <douglaspo_97@outlook.com>
2022-10-16 20:58:26 -0300

    chore: init project, config tailwindcss and storybook

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-5xl text-violet-500">Hello world!</h1>
+      
+      <button className="bg-violet-500 font-medium px-8 py-2 rounded text-white hover:bg-violet-600">
+        Enviar
+      </button>
+    </>
+  )
+}
+
