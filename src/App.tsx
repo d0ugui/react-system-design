911b5764482e2163c37c875ee2356e28ebc3b561
Douglas Oliveira <douglaspo_97@outlook.com>
2022-10-16 21:22:03 -0300

    style: creating design tokens

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import './styles/global.css';
 export function App() {
   return (
     <>
-      <h1 className="font-bold text-5xl text-violet-500">Hello world!</h1>
+      <h1 className="font-bold text-2xl text-violet-500">Hello world!</h1>
       
-      <button className="bg-violet-500 font-medium px-8 py-2 rounded text-white hover:bg-violet-600">
+      <button className="bg-cyan-500 font-medium px-8 py-2 rounded text-white hover:bg-cyan-300">
         Enviar
       </button>
     </>
