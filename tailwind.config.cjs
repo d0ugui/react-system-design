081068a9e7700deeef3377adab68f6cd5a2df630
Douglas Oliveira <douglaspo_97@outlook.com>
2022-10-18 19:11:48 -0300

    feat: create components and stories

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,14 +13,23 @@ module.exports = {
       '2xl': 32
     },
     colors: {
-      'gray-900': '#121214',
-      'gray-800': '#202024',
-      'gray-400': '#7C7C8A',
-      'gray-200': '#C4C4CC',
-      'gray-100': '#E1E1E6',
+      transparent: 'transparent',
 
-      'cyan-500': '#81D8F7',
-      'cyan-300': '#9BE1FB'
+      'black': '#000',
+      'white': '#FFF',
+
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7C7C8A',
+        200: '#C4C4CC',
+        100: '#E1E1E6'
+      },
+
+      cyan: {
+        500: '#81D8F7',
+        300: '#9BE1FB'
+      },
     },
     extend: {
       fontFamily: {
